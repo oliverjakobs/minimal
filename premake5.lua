0266463c54ed2c7293fff7bf5867fb1341461099
oliverjakobs <31634843+oliverjakobs@users.noreply.github.com>
2021-05-03 19:32:23 +0200

    modified minimal to submodule

--- premake5.lua
+++ premake5.lua
@@ -1,57 +1,29 @@
-workspace "Minimal"
-	architecture "x64"
-	startproject "Minimal"
-
-	configurations
-	{
-		"Debug",
-		"OptimizedDebug",
-		"Release"
-	}
-
-outputDir = "%{cfg.buildcfg}"   
-
 project "Minimal"
-	location "Minimal"
-	kind "ConsoleApp"
+	kind "StaticLib"
 	language "C"
-	cdialect "C99"
-	staticruntime "On"
 	
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
 
 	files
 	{
-		--Config
-		"%{prj.name}/config.ini",
-		--Source
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.c"
-	}
-
-	links
-	{
-		"opengl32"
+		"src/**.h",
+		"src/**.c"
 	}
 
 	includedirs
 	{
-		"%{prj.name}/src",
+		"src",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
+		staticruntime "On"
 		defines { "_CRT_SECURE_NO_WARNINGS" }
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "On"
-		
-	filter "configurations:OptimizedDebug"
-		runtime "Debug"
-		symbols "On"
-		optimize "On"
 
 	filter "configurations:Release"
 		runtime "Release"
